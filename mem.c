50d6c387eba05992efe276b2d09eebf72016392e
shanrenmiao <87079678+shanrenmiao@users.noreply.github.com>
2021-07-11 21:42:44 +0800

    Update mem.c

--- mem.c
+++ mem.c
@@ -6,7 +6,13 @@ int main()
 {
   unsigned i=0;
     while(1){
+      if(i>=10194){
+          printf("Can't allocate more memory! Current allocated: %uM\n", i);
+          sleep(10);
+        continue;
+      }
       void *ptr = malloc(1024*1024);
+      
       if(ptr) {
           memset(ptr, 0xff, 1024*1024);
           i++;
